80abf152a76c917820c09b9db75e0b45e356c9f4
Noor Chasib <noorchasib@gmail.com>
2023-03-01 20:07:50 -0800

    create navbar, use enums and add mediaquery breakpoints

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import Navbar from "@/scenes/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
 function App() {
-	return <div className="app">app</div>;
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopofPage, setIsTopofPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopofPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else {
+        setIsTopofPage(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopofPage={isTopofPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  );
 }
 
 export default App;
